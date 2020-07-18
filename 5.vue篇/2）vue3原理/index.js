828136ed323ec31f796c8b95ce4f4d97df14d589
JsRicardo <502918488@qq.com>
2020-07-18 23:14:53 +0800

    reactive

--- 5.vue篇/2）vue3原理/index.js
+++ 5.vue篇/2）vue3原理/index.js
@@ -49,7 +49,6 @@ function patch(n1, n2) {
                 elm.setAttribute(key, newValue)
             }
         }
-
         // 删除了属性
         for (const key in oldProps) {
             if (!(key in newProps)) {
@@ -57,9 +56,54 @@ function patch(n1, n2) {
                 elm.removeAttribute(key)
             }
         }
-
         // 比较children
-        
+        const oldChildren = n1.children
+        const newChildren  = n2.children
+        if (typeof newChildren === 'string') {
+            
+            if (typeof oldChildren === 'string') {
+                // 两个节点都是字符节点，内不同时，修改内容
+                if (newChildren !== oldChildren) {
+                    elm.textContent = newChildren
+                }
+            } else {
+                // 新节是字符 旧节点是 数组 直接替换
+                elm.innerHTML = newChildren
+            } 
+
+        } else if (Array.isArray(newChildren)) {
+            if (typeof oldChildren === 'string') {
+                // 旧节点只是字符 新节点是数组 挂载新节点
+                elm.innerHTML = ''
+                newChildren.forEach(child => {
+                    mount(child, elm)
+                })
+            } else if (Array.isArray(oldChildren)) {
+                // 如果两个节点都是数组  这里vue中用到key的模式去判断是不是同一个元素
+                // 假设没有key  我们只比较两个数组的 index 相同的部分
+                const commonLength = Math.min(newChildren.length, oldChildren.length)
+
+                for(let i = 0; i < commonLength; i++){
+                    // 比较一下 公共部分的 每一个child
+                   patch(oldChildren[i], newChildren[i])
+                }
+
+                // 接下来比较一下差异部分
+                if (newChildren.length > oldChildren.length) {
+                    // 新的子节点多一些，挂载新的子节点
+                    newChildren.slice(oldChildren.length).forEach(child => {
+                        mount(child, elm)
+                    })
+                }
+
+                if (newChildren.length < oldChildren.length) {
+                    // 新的子节点少一些  删除了子节点
+                    oldChildren.slice(newChildren.length).forEach(child => {
+                        elm.removeChild(child.elm)
+                    })
+                }
+            }
+        }
     } else {
         // 如果不是的话 就要进行节点替换了，但是又不能直接替换，里面比较复杂
     }
