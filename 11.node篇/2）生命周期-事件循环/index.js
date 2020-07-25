559d21e6bf1e4ef57e190108a3b52a70fa616d6b
JsRicardo <502918488@qq.com>
2020-07-26 00:35:01 +0800

    node 事件循环

--- 11.node篇/2）生命周期-事件循环/index.js
+++ 11.node篇/2）生命周期-事件循环/index.js
@@ -1,5 +1,18 @@
 const fs = require('fs')
+const { setImmediate } = require('timers')
 
-fs.stat("https://user-gold-cdn.xitu.io/2020/7/24/1737fe6072145007?w=690&h=378&f=jpeg&s=28669", (err, s) => {
-    console.log(err, s)
+
+setTimeout(() => {
+    console.log(1)
+}, 0)
+
+fs.readFile('./index.js', (err, data) => {
+    console.log(2)
 })
+
+setImmediate(() => {
+    console.log(3)
+})
+
+console.log(4)
+
