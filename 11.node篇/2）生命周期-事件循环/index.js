fd92192f1a8cf278c381f6ff0d667a192b6a2265
liming <liming@youxuepai.com>
2020-07-27 10:46:24 +0800

    node事件循环

--- 11.node篇/2）生命周期-事件循环/index.js
+++ 11.node篇/2）生命周期-事件循环/index.js
@@ -2,17 +2,57 @@ const fs = require('fs')
 const { setImmediate } = require('timers')
 
 
-setTimeout(() => {
-    console.log(1)
-}, 0)
+// setTimeout(() => {
+//     console.log(1)
+// }, 0)
 
-fs.readFile('./index.js', (err, data) => {
-    console.log(2)
-})
+// fs.readFile('./index.js', (err, data) => {
+//     setTimeout(() => {
+//         console.log(1)
+//     }, 0)
+//     setImmediate(() => {
+//         console.log(2)
+//     })
+// })
+
+// setImmediate(() => {
+//     console.log(3)
+// })
+
+// console.log(4)
 
 setImmediate(() => {
-    console.log(3)
+    console.log(1)
 })
 
-console.log(4)
+Promise.resolve().then(() => {
+    console.log(2)
+    process.nextTick(() => {
+        console.log(3)
+    })
+})
 
+process.nextTick(() => {
+    console.log(4)
+    process.nextTick(() => {
+        console.log(5)
+    })
+})
+
+console.log(6)
+
+const { EventEmitter } = require('events')
+
+const EE = new EventEmitter
+
+const fn = () => {
+    console.log('add')
+}
+
+EE.on('add', fn)
+
+EE.once('once', () => {
+    console.log('once')
+})
+
+EE.off(fn, 'add')
